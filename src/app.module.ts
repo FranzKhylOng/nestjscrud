5a4a92874033be853d61bcdf8673d05013d20ec1
Franz Khyl Ong <117539969+FranzKhylOng@users.noreply.github.com>
2023-07-12 09:17:20 +0800

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot('mongodb://localhost/nestjs'),
     UserModule,
     ProductModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
