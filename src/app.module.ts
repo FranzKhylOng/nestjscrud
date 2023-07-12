cca48b563338399c4fd10a368ac026d984df2485
Franz Khyl Ong <117539969+FranzKhylOng@users.noreply.github.com>
2023-07-12 09:21:21 +0800

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
-import { OrderModule } from './order/product.module';
+import { OrderModule } from './order/order.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
-import { OrderModel } from './order.model';
 import { OrderService } from './order.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './order.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
   controllers: [OrderController],
-  providers: [OrderModel, OrderService],
+  providers: [OrderService],
 })
 export class OrderModule {}
